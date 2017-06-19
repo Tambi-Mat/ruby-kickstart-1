a52190db7964cf746a8627432a542dd234c3b981
Tambi-Mat <caicaratambi@gmail.com>
2017-06-19 15:23:52 +0100

    my_solution 2:11 failed

--- session2/3-challenge/11_classes.rb
+++ session2/3-challenge/11_classes.rb
@@ -20,3 +20,41 @@
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+
+class BeerSong
+  attr_accessor "number"
+
+  def initialize(number)
+    @number = number
+    if @number < 0
+      @number == 0
+    else @number > 99
+      @number == 99
+    end
+  end
+
+  def print_song
+    @number.downto(1) do |n|
+      puts " #{num_to_let n} #{bottle n} of beer on the wall,
+      #{num_to_let n} #{bottle n} of beer, Take one down, pass it around,
+      #{num_to_let n - 1} #{bottle n - 1} of beer on the wall."
+    end
+  end
+
+  def bottle(n)
+    # change bottles to bottle for number one
+    if n == 1 then 'bottle' else 'bottles' end
+  end
+
+  def num_to_let(n)
+    # change integers to words
+    if 0 <= n && n <= 19
+      %w(zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)[n]
+    elsif n % 10 == 0
+      %w(zero ten twenty thirty forty fifty sixty seventy eighty ninety)[n/10]
+    else
+      "#{num_to_let n/10*10}-#{num_to_let n%10}".downcase
+    end.capitalize
+  end
+
+end
