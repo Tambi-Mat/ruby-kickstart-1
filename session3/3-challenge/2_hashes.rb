aaab39cc8838a5af2ea28be6df4ea53d2d6d66c3
Tambi-Mat <caicaratambi@gmail.com>
2017-06-24 22:26:27 +0100

    hashes and arrays_failed

--- session3/3-challenge/2_hashes.rb
+++ session3/3-challenge/2_hashes.rb
@@ -8,6 +8,29 @@
 # staircase 4  # => {1 => [], 3 => [2]}
 # staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
 
+def staircase(*n)
+    key_odd = Array.new
+    value_even = Array.new
 
+    (n).map do |x|
+        if x % 2 == 0
+            value_even << x
+        else
+            key_odd << x
+        end
+    end
+    # create hash key from each number in array key_odd with an empty array as a value
+    hash = Hash.new
+    key_odd.each { |x| hash[x] = [] }
 
-
+    # iterate over the hash with key as an odd number
+    # and value as an empty array and add all numbers up to the key (odd number)
+    # that are even numbers
+    hash.each do |key, value|
+        for n in (1..key)
+            if n.even?
+                value << n
+            end
+        end
+    end
+end
