9c9c527fdbd6255eaca3a45c2e96dc4aaa3578c1
Tambi-Mat <caicaratambi@gmail.com>
2017-06-24 23:02:29 +0100

    hashes with keys grouped by strings

--- session3/3-challenge/3_hashes.rb
+++ session3/3-challenge/3_hashes.rb
@@ -6,3 +6,10 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+def word_count(string)
+    hash = Hash.new(0) # this arguement (0) for the hash is important! WHY???
+    string.split.each {|x, y| hash[x.downcase] += 1}
+    hash
+end
+p word_count("The dog and the cat love to fight")
