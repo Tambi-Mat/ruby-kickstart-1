9dac50dc1c6d1e39d70a522bbebe01b13eb54760
Tambi-Mat <caicaratambi@gmail.com>
2017-06-26 09:04:20 +0100

    strings split using each_with_index

--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -5,5 +5,11 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
-
+	new_str = []
+	string.split("").each_with_index do |key, value|
+		if key == "r" or key == "R"
+			new_str << string[value+1]
+		end
+	end
+	return new_str.join
 end
